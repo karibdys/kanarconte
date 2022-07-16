4133f21e5bb547e976d795a6ec99b8ff3b7985c1
Manu Mora <karibdys@gmail.com>
2022-07-16 10:38:11 +0200

    Inicio aplicación back

---
+++ src/test/java/karibdys/Kanarconte/KanarconteApplicationTests.java
@@ -0,0 +1,13 @@
+package karibdys.Kanarconte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanarconteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
